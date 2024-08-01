3731ab7422f4fca687d035a59ae9ebc7597c49fb
mariecharlotte.leroy62@gmail.com <mariecharlotte.leroy62@gmail.com>
2024-08-01 02:30:32 +0200

    MAJ aout 2024

--- symfony/config/bundles.php
+++ symfony/config/bundles.php
@@ -21,4 +21,5 @@ return [
     SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
+    Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
 ];
